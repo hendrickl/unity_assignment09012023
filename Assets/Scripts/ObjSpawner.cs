14bc4d8e83d5614c02e8fcc831496328c98f49a7
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-10 09:46:09 +0100

    Instantiate object front of camera with random x y

--- Assets/Scripts/ObjSpawner.cs
+++ Assets/Scripts/ObjSpawner.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ObjSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float _distanceToCam = 5.0f;
+    [SerializeField] private GameObject _objtToSpawn;
+    [SerializeField] private Vector3 _spawnPosition;
+
     void Start()
     {
-        
+        InvokeRepeating("InstantiateObjFrontOf", 3f, 3f);
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void InstantiateObjFrontOf()
+    {
+        _spawnPosition = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), _distanceToCam);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(_spawnPosition);
+
+        GameObject prefab = Instantiate(_objtToSpawn, worldPosition, Quaternion.identity);
     }
 }
